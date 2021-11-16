6a2791c23bfd6efc900d362cf5ce198a5f0bf249
dimkael <53169743+dimkael@users.noreply.github.com>
2021-11-16 23:45:56 +0300

    added templates and db models

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
+from .models import Project
 
-# Create your views here.
+
+projectsList = [
+    {
+        'id': '1',
+        'title': 'Ecommerce Website',
+        'description': 'Fully func ecommerce website'
+    },
+    {
+        'id': '2',
+        'title': 'Portfolio Website',
+        'description': 'This was a project where I built out my portfolio'
+    },
+    {
+        'id': '3',
+        'title': 'Social Network',
+        'description': 'Now i am trying to implemented social network'
+    }
+]
+
+
+def projects(request):
+    projects = Project.objects.all()
+    context = {'projects': projects}
+    return render(request, 'projects/projects.html', context)
+
+
+def project(request, pk):
+    proj = Project.objects.get(id=pk)
+    context = {'project': proj}
+    return render(request, 'projects/singleproject.html', context)
