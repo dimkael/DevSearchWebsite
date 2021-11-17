acf258afb6fece85ad1660b3c6d76dcda02f66b9
dimkael <53169743+dimkael@users.noreply.github.com>
2021-11-17 18:47:28 +0300

    added edit and delete project methods

--- projects/views.py
+++ projects/views.py
@@ -1,12 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Project
 from .forms import ProjectForm
 
 
 def create_project(request):
     form = ProjectForm()
+
+    if request.method == 'POST':
+        form = ProjectForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('projects')
+
     context = {'form': form}
     return render(request, 'projects/project_form.html', context)
+
+
+def update_project(request, pk):
+    proj = Project.objects.get(id=pk)
+    form = ProjectForm(instance=proj)
+
+    if request.method == 'POST':
+        form = ProjectForm(request.POST, instance=proj)
+        if form.is_valid():
+            form.save()
+            return redirect('projects')
+
+    context = {'form': form}
+    return render(request, 'projects/project_form.html', context)
+
+
+def delete_project(request, pk):
+    proj = Project.objects.get(id=pk)
+
+    if request.method == 'POST':
+        proj.delete()
+        return redirect('projects')
+
+    context = {'object': proj}
+    return render(request, 'projects/delete_template.html', context)
 
 
 def projects(request):
